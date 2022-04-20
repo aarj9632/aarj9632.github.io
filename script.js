7d466799c75d54cf55e809ade594dff015c799a8
Aayushman Raj <aarj9632@gmail.com>
2022-04-20 23:36:19 +0530

    Web Development

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// document.oncontextmenu = new Function("return false");
+// document.oncontextmenu = function right_click() {
+//     alert("Right Click is disabled, Press F12 to inspect.")
+//     return false;
+// }
 
 document.onkeydown = function (e) {
 
-    // disable U key
     if(e.ctrlKey && e.keyCode == 85) {
-        alert("Ctrl+U is disabled.")
+        alert("Ctrl+U (View Page Source) is disabled.")
         return false;
     }
 }
