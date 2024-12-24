98d40c10855fdf5f67fae2d0af07ab75dd31444f
Aayushman Raj <aarj9632@gmail.com>
2024-12-24 13:32:40 +0530

    hi

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,5 +1,4 @@
 function contactme() {
-    document.querySelector(".footer").style.cssText = "border: 5px dashed black; padding:10px; border-radius: 20px";
+    document.querySelector(".footer").style.cssText = "border: 5px dashed gray; padding:10px; border-radius: 20px; transition: 0.2s";
+    document.querySelector(".socialmedia").style.cssText = "border: 5px dashed gray; padding:10px; border-radius: 20px; transition: 0.2s";
 }
-
-// document.onclick.getElementsByClassName()
